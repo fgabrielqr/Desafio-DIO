6d6131f04a6e641907f8158af851ae0fb2fb6105
fgabrielqr <fgabrielqr@gmail.com>
2023-11-22 10:16:54 -0300

    feat: :zap: atividade finalizada

--- index.js
+++ index.js
@@ -24,3 +24,32 @@
 
 // Ao final deve se exibir uma mensagem:
 // "O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+
+// Captura do nome e XP do herói
+let nomeHeroi = "Júnior"
+let xpHeroi = 12000
+
+// Determinação do nível com base na XP
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibição da mensagem final
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
