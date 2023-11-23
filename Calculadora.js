66e243d99d55daa5957da5e0e4e97acf1088e5ac
fgabrielqr <fgabrielqr@gmail.com>
2023-11-23 10:37:44 -0300

    feat: :sparkles: Calculadora de partidas Rankeadas

---
+++ Calculadora.js
@@ -0,0 +1,61 @@
+// # 2️⃣ Calculadora de partidas Rankeadas
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+// - Funções
+
+// ## Objetivo:
+
+// Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
+// depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
+
+// Se vitórias for menor do que 10 = Ferro
+// Se vitórias for entre 11 e 20 = Bronze
+// Se vitórias for entre 21 e 50 = Prata
+// Se vitórias for entre 51 e 80 = Ouro
+// Se vitórias for entre 81 e 90 = Diamante
+// Se vitórias for entre 91 e 100= Lendário
+// Se vitórias for maior ou igual a 101 = Imortal
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+// "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+function calcularSaldoVitorias(vitorias, derrotas) {
+    return vitorias - derrotas;
+}
+
+function determinarNivel(vitorias) {
+    if (vitorias < 10) {
+        return "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        return "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        return "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        return "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        return "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        return "Lendário";
+    } else {
+        return "Imortal";
+    }
+}
+
+function exibirMensagem(saldoVitorias, nivel) {
+    console.log("O Herói tem um saldo de " + saldoVitorias + " está no nível de " + nivel);
+}
+
+// Exemplo de uso
+let vitorias = 100;
+let derrotas = 5;
+
+let saldoVitorias = calcularSaldoVitorias(vitorias, derrotas);
+let nivel = determinarNivel(vitorias);
+exibirMensagem(saldoVitorias, nivel);
+
